c14647815564a1de41ae77f99be3db19cb0a1c30
kaymohh <thrilltim@gmail.com>
2025-05-25 17:29:08 +0100

    added dynamic url for movie

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('login', views.login, name="login"),
     path('signup', views.signup, name="signup"),
+    path('movie/<str:pk>/', views.movie, name='movie')
+
 
 ]
