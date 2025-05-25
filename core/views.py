e141e68c58bfac30d900be2d487dc6971bef0bbf
kaymohh <thrilltim@gmail.com>
2025-05-25 17:33:02 +0100

    updating movie views with login decorator

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def index (request):
     return render(request, 'index.html')
     
@@ -21,6 +23,17 @@ def login(request):
             return redirect('login')
     
     return render(request, 'login.html')
+
+@login_required(login_url='login')
+def movie(request, pk):
+    movie_uuid = pk
+    movie_details = Movie.objects.get(uu_id=movie_uuid)
+
+    context = {
+        'movie_details': movie_details
+    }
+
+    return render(request, 'movie.html', context)
 
 def signup(request):
     if request.method == 'POST':
